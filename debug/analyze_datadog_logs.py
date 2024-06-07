8d3d19b6616b7766241cb0bd565412fb4523afb2
Liam Park <liam@asleep.ai>
2024-06-08 00:44:26 +0900

    update datadog logs scripts

--- debug/analyze_datadog_logs.py
+++ debug/analyze_datadog_logs.py
@@ -5,7 +5,10 @@ from config.config_logging import logger
 
 
 def extract_float(value):
-    pass
+    if isinstance(value, str):
+        return float(value.split(" ")[0])
+
+    return value
 
 
 def load_logs_from_file(fileName: str):
@@ -15,16 +18,29 @@ def load_logs_from_file(fileName: str):
     return {
         "function": [log["function"] for log in logs],
         "file": [log["file"] for log in logs],
-        "duration": [log["duration"] for log in logs],
+        "duration": [extract_float(log["duration"]) for log in logs],
     }
 
 
 def main():
-    fileName0 = "2024-06-07.pkl"
+    fileName0 = "2024-06-06.pkl"
     logs0 = load_logs_from_file(fileName0)
     frame0 = pl.DataFrame(logs0, schema={"function": pl.String, "file": pl.String, "duration": pl.Float32})
 
-    logger.info(logs0)
+    result0 = (
+        frame0.group_by("function", "file")
+        .agg(
+            pl.count("function").alias("call_count"),
+            pl.col("duration").sum().alias("total_duration"),
+            pl.col("duration").mean().alias("mean_duration"),
+            pl.col("duration").max().alias("max_duration"),
+            pl.col("duration").min().alias("min_duration"),
+            pl.col("duration").std().alias("std_duration"),
+        )
+        .sort("file")
+    )
+
+    result0.write_excel("2024-06-06.xlsx")
 
 
 main()
--- debug/dump_datadog_logs.py
+++ debug/dump_datadog_logs.py
@@ -68,17 +68,17 @@ def write_file(started_at: datetime, result: List[dict]):
 
 
 def main():
-    started_at0 = get_datetime(2024, 6, 7, 5, 0, 0)
-    ended_at0 = get_datetime(2024, 6, 7, 6, 0, 0)
+    started_at0 = get_datetime(2024, 6, 6, 5, 0, 0)
+    ended_at0 = get_datetime(2024, 6, 6, 8, 0, 0)
     result0 = get_datadog_logs(started_at0, ended_at0)
-    write_file(started_at0, ended_at0)
+    write_file(started_at0, result0)
 
-    started_at1 = get_datetime(2024, 6, 7, 7, 0, 0)
-    ended_at1 = get_datetime(2024, 6, 7, 8, 0, 0)
-    result1 = get_datadog_logs(started_at1, ended_at1)
+    # started_at1 = get_datetime(2024, 6, 7, 5, 0, 0)
+    # ended_at1 = get_datetime(2024, 6, 7, 8, 0, 0)
+    # result1 = get_datadog_logs(started_at1, ended_at1)
 
-    result = result0 + result1
-    write_file(started_at1, result)
+    # result = result0
+    # write_file(started_at0, result)
 
     print("Done!")
 
