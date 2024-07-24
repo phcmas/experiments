e5930215d4590ef83a5fa20a88b300fff6e9c077
Liam Park <liam@asleep.ai>
2024-07-24 22:42:56 +0900

    conver logits 수정

--- debug/convert_logits.py
+++ debug/convert_logits.py
@@ -1,19 +1,23 @@
 import base64
+import io
+import json
 import time
 import numpy as np
 import struct
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 
 
-class InputPayload(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
+class InputPayloadEncoded(BaseModel):
     session_id: str
     db_type: str = "MAIN"
 
-    sleep_stage_logits: np.ndarray
-    osa_logits: np.ndarray
-    snoring_logits: np.ndarray
+    logits_length: int
+    sleep_stage_logits: str
+    osa_logits: str
+    snoring_logits: str
+
+    model_name: str
+    model_version: str
 
     callback_url: str = "http://test-api.asleep.ai/data"
     callback_version: str = "V2"
@@ -28,6 +32,10 @@ def timeit(func):
         return result
 
     return wrapper
+
+
+def encode_payload_base64():
+    pass
 
 
 def generate_logits(type_count: int, stage_count: int, decimal_point: int = -1) -> np.ndarray:
@@ -104,13 +112,26 @@ def measure_the_time_of_conversion(hour: int):
 
 def array_to_base64_2d(array):
     # Convert the 2D list of floats to a 1D byte array
-    byte_array = b"".join(struct.pack("f", item) for row in array for item in row)
+    # byte_array = b"".join(struct.pack("f", item) for row in array for item in row)
+
+    buf = io.BytesIO()
+    np.save(buf, array)
+    binary = buf.getvalue()
 
     # Encode the byte array to Base64
-    base64_encoded = base64.b64encode(byte_array)
+    base64_encoded = base64.b64encode(binary)
 
     # Convert the Base64 bytes to a string
     return base64_encoded.decode("utf-8")
+
+
+def base64_to_ndarray(base64_string):
+    decoded_binary = base64.b64decode(base64_string)
+
+    buf = io.BytesIO(decoded_binary)
+    decoded_array = np.load(buf)
+
+    return decoded_array
 
 
 def array_to_utf8_2d(array):
@@ -132,12 +153,11 @@ def base64_to_array_2d(base64_string, num_rows, num_cols):
     return [floats[i * num_cols : (i + 1) * num_cols] for i in range(num_rows)]
 
 
-def encode_and_decode_logits_base64():
+def encode_logits():
     sleep_logits = generate_logits(4, 1200)
     osa_logits = generate_logits(3, 1200)
     snoring_logits = generate_logits(2, 1200)
 
-    # Encode the list of floats to Base64
     sleep_logits_base64 = array_to_base64_2d(sleep_logits)
     osa_logits_base64 = array_to_base64_2d(osa_logits)
     snoring_logits_base64 = array_to_base64_2d(snoring_logits)
@@ -147,14 +167,37 @@ def encode_and_decode_logits_base64():
     print(f"snoring logits base64: {len(snoring_logits_base64)}")
     print(f"total: {len(snoring_logits_base64) + len(osa_logits_base64) + len(snoring_logits_base64)}")
 
-    # Decode the Base64 string back to the list of floats
-    sleep_logits_decoded = base64_to_array_2d(sleep_logits_base64, num_rows=1200, num_cols=4)
-    osa_logits_decoded = base64_to_array_2d(osa_logits_base64, num_rows=1200, num_cols=3)
-    snoring_logits_decoded = base64_to_array_2d(snoring_logits_base64, num_rows=1200, num_cols=2)
+    sleep_logits_utf8 = array_to_utf8_2d(sleep_logits)
+    osa_logits_utf8 = array_to_utf8_2d(osa_logits)
+    snoring_logits_utf8 = array_to_utf8_2d(snoring_logits)
 
-    print(f"sleep logits decoded: {sleep_logits_decoded}")
-    print(f"osa logits decoded: {osa_logits_decoded}")
-    print(f"snoring logits decoded: {snoring_logits_decoded}")
+    print(f"sleep logits utf8: {len(sleep_logits_utf8)}")
+    print(f"osa logits utf8: {len(osa_logits_utf8)}")
+    print(f"snoring logits utf8: {len(snoring_logits_utf8)}")
+    print(f"total: {len(snoring_logits_utf8) + len(osa_logits_utf8) + len(snoring_logits_utf8)}")
+
+
+def create_input_payload():
+    sleep_logits = generate_logits(4, 1200)
+    osa_logits = generate_logits(3, 1200)
+    snoring_logits = generate_logits(2, 1200)
+
+    sleep_logits_base64 = array_to_base64_2d(sleep_logits)
+    osa_logits_base64 = array_to_base64_2d(osa_logits)
+    snoring_logits_base64 = array_to_base64_2d(snoring_logits)
+
+    return InputPayloadEncoded(
+        session_id="20240715164914_uw0jy",
+        db_type="MAIN",
+        logits_length=len(sleep_logits),
+        sleep_stage_logits=sleep_logits_base64,
+        osa_logits=osa_logits_base64,
+        snoring_logits=snoring_logits_base64,
+        model_name="pp",
+        model_version="v2.0.3",
+        callback_url="https://test-api.asleep.ai/data",
+        callback_version="V2",
+    )
 
 
 def save_and_load_binary():
@@ -194,8 +237,16 @@ def main():
 
     # measure_the_size_for_logits(48)
     # measure_the_time_of_conversion(48)
+    # save_and_load_binary()
 
-    save_and_load_binary()
+    # encode_logits()
+
+    input = create_input_payload()
+
+    print(len(str(input.model_dump())))
+
+    with open("input.json", "w") as file:
+        json.dump(input.model_dump(), file, indent=4)
 
 
 main()
