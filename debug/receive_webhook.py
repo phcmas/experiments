d4bf08718be4ff02b190a0b52499cc645532a402
Liam Park <liam@asleep.ai>
2024-11-19 11:08:23 +0900

    Modify receive webhook

--- debug/receive_webhook.py
+++ debug/receive_webhook.py
@@ -10,18 +10,8 @@ app = FastAPI(
 )
 
 
-@app.post("/inference-complete", tags=["webhook"])
-def receive_inference_complete(request: dict):
-    file_path = "webhook.log"
-
-    with open(file_path, "a") as f:
-        f.write(str(request) + "\n")
-
-    return {"message": "success"}
-
-
-@app.post("/session-complete", tags=["webhook"])
-def receive_session_complete(request: dict):
+@app.post("/webhook", tags=["webhook"])
+def receive_webhook(request: dict):
     file_path = "webhook.log"
 
     with open(file_path, "a") as f:
