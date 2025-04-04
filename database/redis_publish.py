21ec423287df66162d36d9c5ce4c36e8d316b15c
Liam Park <liam@asleep.ai>
2025-04-04 17:58:44 +0900

    Modify the publish messages

--- database/redis_publish.py
+++ database/redis_publish.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from redis.exceptions import RedisError
@@ -10,14 +11,14 @@ redis = get_redis_connection()
 logger = logging.getLogger(__name__)
 
 
-def publish_message(channel, message):
+def publish_message(channel: str, message: dict):
     try:
-        redis.publish(channel, message)
+        redis.publish(channel, json.dumps(message))
         logger.info(f"published message: {message} to channel: {channel}")
     except RedisError as e:
         logger.error(f"failed to publish message: {e}")
 
 
 if __name__ == "__main__":
-    message = "Hello, Redis"
+    message = {"session_id": "20250404042603_moxje", "requested_at": "2023-10-01T12:00:00Z"}
     publish_message("test_channel", message)
